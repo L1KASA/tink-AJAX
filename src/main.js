aeaebcb42e747239c6e42389a05122485ffb2dd0
L1KASA <likas2280@mail.ru>
2023-01-05 13:39:50 +0400

    changes

--- src/main.js
+++ src/main.js
@@ -1,178 +1,234 @@
-import '../node_modules/normalize.css/normalize.css';
-import './style.css';
 
-/*
- * Задание:
- * Реализовать ToDoList приложение которое будет отображать список всех дел
- * Можно: просмотреть список всех дел, добавить todo и удалить, а так же изменить
- *
- * */
-
-class ApiService {
-    fetchAllTodos() {
-        return fetch('/api/todos').then((res) => res.json());
-    }
-
-    create(data) {
-        return fetch('/api/todos', {
+class ApiService{
+    create(data){
+        return fetch('https://jsonplaceholder.typicode.com/posts',{
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json; charset=UTF-8'
             },
-            body: JSON.stringify(data),
-        }).then((res) => {
-            return res.json();
-        });
+            body: JSON.stringify(data)
+        }).then((res) => res.json()).catch((e) => alert('error message: e'));
     }
-
-    remove(id) {
-        return fetch(`/api/todos/${id}`, {
-            method: 'DELETE',
-        });
+    edit(data, updId){
+        const id = updId;
+        return fetch('https://jsonplaceholder.typicode.com/posts/' + id,{
+            method: 'PATCH',
+            body: JSON.stringify(data),
+            headers:{
+                'Content-type': 'application/json; charset=UTF-8',
+              },
+        }).then((res) => res.json()).catch((e) => alert('error message: ' + e));
+    }
+    getName(id){
+        return fetch('https://jsonplaceholder.typicode.com/users/' + id).then((res) => res.json()).then((json) => json.username).catch((e) => alert(`error message: ${e}`));
+    }
+    remove(id){
+        return fetch('https://jsonplaceholder.typicode.com/posts/' + id,{
+            method:'DELETE',
+        }).catch((e) => alert(`error message: ${e}`));
     }
 }
 
-// Отвечает за рендер
-class TodoService {
-    toToList;
-
-    constructor(api) {
+class PostService{
+    updId;
+    constructor(api){
         this.api = api;
-        this.toToList = window.document.querySelector('.todo-list');
+        this.postsList = window.document.querySelector('.posts__container');
         this._handleRemove = this._handleRemove.bind(this);
+        this.openEdit = this.openEdit.bind(this);
+        this._onEdit = this._onEdit.bind(this);
+        this.editModal = document.querySelector('.edit-modal');
+
+    }
+    addPost(name, title, body, id){
+        this.postsList.append(this._createPost(name, title, body, id));
     }
 
-    addTodo(number, title, description) {
-        this.toToList.append(this._createTodo(number, title, description));
-    }
-
-    _createTodo(number, title, description) {
+    _createPost(name, title, body, id){
         const container = document.createElement('div');
-        container.classList.add('todo-list__item');
-        container.cardId = number;
-        container.classList.add('card');
-        const header = document.createElement('div');
-        header.classList.add('card__header');
-        const content = document.createElement('div');
-        content.classList.add('card__content');
+        container.classList.add('posts__content');
+        container.id = id;
+        const userName = document.createElement('div');
+        userName.classList.add('username');
+        const nameElem = document.createElement('h3');
+        nameElem.append(document.createTextNode(name));
 
-        const numberEl = document.createElement('h3');
-        numberEl.append(document.createTextNode(number));
-        numberEl.classList.add('card__number');
+        const info = document.createElement('div');
+        info.classList.add('info');
+        const titleEl = document.createElement('div');
+        titleEl.classList.add('title');
+        const titleHead = document.createElement('h4');
+        titleHead.append(document.createTextNode(title));
+        const bodyEl = document.createElement('div');
+        bodyEl.classList.add('body');
+        const bodyHead = document.createElement('p');
+        bodyHead.append(document.createTextNode(body));
 
-        const titleEl = document.createElement('h3');
-        titleEl.append(document.createTextNode(title));
-        titleEl.classList.add('card__title');
+        const delContainer = document.createElement('div');
+        delContainer.classList.add('delete-button')
+        const delBtn = document.createElement('button');
+        delBtn.append(document.createTextNode('Удалить'));
 
-        content.append(document.createTextNode(description));
-        content.classList.add('card__description');
+        const editContainer = document.createElement('div');
+        editContainer.classList.add('edit-button')
+        const editBtn = document.createElement('button');
+        editBtn.append(document.createTextNode('Редактировать'));
 
-        const btnEl = document.createElement('button');
-        btnEl.append(document.createTextNode('x'));
-        btnEl.classList.add('card__remove');
+        titleEl.append(titleHead);
+        bodyEl.append(bodyHead);
+        delContainer.append(delBtn);
+        editContainer.append(editBtn);
+        info.append(titleEl);
+        info.append(bodyEl);
+        userName.append(nameElem);
 
-        header.append(numberEl);
-        header.append(titleEl);
-        header.append(btnEl);
+        container.append(userName);
+        container.append(info);
+        container.append(delContainer);
+        container.append(editContainer);
 
-        container.append(header);
-        container.append(content);
-        btnEl.addEventListener('click', this._handleRemove);
+        delBtn.addEventListener('click', this._handleRemove);
+        editBtn.addEventListener('click', this.openEdit);
 
         return container;
     }
 
-    _handleRemove(event) {
-        const card = event.target.parentElement.parentElement;
-        this.api.remove(card.cardId).then((res) => {
-            if (res.status >= 200 && res.status <= 300) {
-                event.target.removeEventListener('click', this._handleRemove);
-                card.remove();
+    _handleRemove(event){
+        const post = event.target.parentElement.parentElement;
+        this.api.remove(post.id).then((res) => {
+            if(res.status >= 200 && res.status <= 300){
+                event.target.removeEventListener('click',this._handleRemove);
+                post.remove();
             }
-        });
+        })
     }
-}
+    openEdit(e){
+        this.editModal.classList.toggle('visually-hidden');
+        const id = e.target.parentElement.parentElement.id;
+        const elem = e.target.parentElement.parentElement;
+        const editBtn = document.querySelector('.on-edit-button');
+        editBtn.addEventListener('click',() => this._onEdit(e, id, elem));
 
-class MainService {
-    constructor(todoService, modalService, api) {
-        this.modalService = modalService;
-        this.api = api;
-        this.todoService = todoService;
-        document.getElementsByClassName('app');
-        this.btn = document.getElementById('addBtn');
-        this.btn.addEventListener('click', (e) => this._onOpenModal(e));
     }
+    closeEdit(){
 
-    fetchAllTodo() {
-        this.api.fetchAllTodos().then((todos) => {
-            todos.forEach((todo) =>
-                this.todoService.addTodo(todo.id, todo.title, todo.description)
-            );
-        });
+        this.editModal.classList.toggle('visually-hidden');
     }
-
-    _onOpenModal() {
-        this.modalService.open();
-    }
-}
-
-class ModalService {
-    constructor(todoService, api) {
-        this.api = api;
-        this.todoService = todoService;
-        this.overlay = document.querySelector('.overlay');
-        this.modal = document.querySelector('.modal');
-
-        this.listener = this.close.bind(this);
-        document
-            .querySelector('.modal svg')
-            .addEventListener('click', this.listener);
-
-        this.submitBtn = document.querySelector('.submit-btn');
-        this.submitBtn.addEventListener('click', this._onCreate.bind(this));
-    }
-
-    open() {
-        this.modal.classList.add('active');
-        this.overlay.classList.add('active');
-    }
-
-    close() {
-        this.modal.classList.remove('active');
-        this.overlay.classList.remove('active');
-    }
-
-    _onCreate(e) {
+    _onEdit(e, id,elem){
         e.preventDefault();
-
         const formData = {};
         const form = document.forms[0];
-
+        const userName = elem.querySelector('.username h3'),
+                  title = elem.querySelector('.info .title h4'),
+                  body = elem.querySelector('.info .body p');
         Array.from(form.elements)
             .filter((item) => !!item.name)
             .forEach((elem) => {
                 formData[elem.name] = elem.value;
             });
-
+        formData['id'] = id;
         if (!this._validateForm(form, formData)) {
             return;
         }
+        const userid = formData['userid'];
+        this.api.edit(formData, id).then((data) => {
+            title.textContent = `${data.title}`;
+            body.textContent = `${data.body}`;
+        }).then(this.api.getName(userid).then((name) => userName.textContent = `${name}`));
+        form.reset();
+        this.closeEdit();
+    }
+    _validateForm(form, formData) {
+        const errors = [];
+        if (formData.userid > 10 || formData.userid <= 0 || isNaN(+formData.userid)) {
+            errors.push('Поле "Ид пользователя" не должно иметь значения больше 10 или меньше 0');
+        }
+        if (!formData.userid.length || !formData.title.length || !formData.body.length) {
+            errors.push('Все поля должны быть заполнены!');
+        }
 
-        this.api.create(formData).then((data) => {
-            this.todoService.addTodo(data.id, data.title, data.description);
-        });
+        if (errors.length) {
+            const errorEl = form.getElementsByClassName('form-errors')[0];
+            errorEl.innerHTML = errors.map((er) => `<div>${er}</div>`).join('');
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+class MainService{
+    constructor(postService, modalService, api){
+        this.modalService = modalService;
+        this.api = api;
+        this.postService = postService;
+        this.addBtn = document.querySelector('.add-button');
+        this.addBtn.addEventListener('click', (e) => this._onOpenModal(e));
+    }
+    init(){
+        console.log('');
+    }
+    _onOpenModal(){
+        this.modalService.open();
+    }
+}
+
+class ModalService{
+    constructor(postService, api) {
+        this.api = api;
+        this.postService = postService;
+        this.addModal = document.querySelector('.add-modal');
+        this.editModal = document.querySelector('.edit-modal');
+        this.editListener = this.closeEdit.bind(this);
+        document.querySelector('.edit-close').addEventListener('click', this.editListener);
+        this.listener = this.close.bind(this);
+        document.querySelector('.add-close').addEventListener('click', this.listener);
+
+        this.submitBtn = document.querySelector('.submit-btn');
+        this.submitBtn.addEventListener('click', this._onCreate.bind(this));
+
+    }
+
+    open(){
+        this.addModal.classList.toggle('visually-hidden');
+    }
+    close(){
+        this.addModal.classList.toggle('visually-hidden');
+    }
+    closeEdit(){
+        this.editModal.classList.toggle('visually-hidden');
+    }
+    _onCreate(e) {
+        e.preventDefault();
+
+        const formData = {};
+        const form = document.forms[1];
+        Array.from(form.elements)
+            .filter((item) => !!item.name)
+            .forEach((elem) => {
+                formData[elem.name] = elem.value;
+            });
+        if (!this._validateForm(form, formData)) {
+            return;
+        }
+        this.api.getName(formData.userid).then((res) => {
+            formData['userid'] = res;
+            return formData;
+        }).then((formData) => this.api.create(formData).then((data) => {
+            this.postService.addPost(data.userid, data.title, data.body, data.id);
+        }));
         form.reset();
         this.close();
     }
 
     _validateForm(form, formData) {
         const errors = [];
-        //вместо if используются отдельные функции-валидаторы
-        if (formData.title.length >= 30) {
-            errors.push('Поле наименование должно иметь не более 30 символов');
+        if (formData.userid > 10 || formData.userid <= 0 || isNaN(+formData.userid)){
+            errors.push('Поле "Ид пользователя" не должно иметь значения больше 10 или меньше 0');
         }
-        if (!formData.description.length) {
-            errors.push('Поле описание должно быть заполнено');
+        if (!formData.userid.length || !formData.title.length || !formData.body.length) {
+            errors.push('Все поля должны быть заполнены!');
         }
 
         if (errors.length) {
@@ -187,8 +243,7 @@ class ModalService {
 }
 
 const api = new ApiService();
-const todoService = new TodoService(api);
-const modalService = new ModalService(todoService, api);
-const service = new MainService(todoService, modalService, api);
-service.fetchAllTodo();
-//todo: изменение не реализовано
+const postService = new PostService(api);
+const modalService = new ModalService(postService, api);
+const service = new MainService(postService, modalService, api);
+service.init();
